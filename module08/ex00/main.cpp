f7fc46b0a97d18fac0b11f233ac2228fdfdb2f59
Marti Marsa Samper <mmarsa-s@car10s5.42barcelona.com>
2025-04-16 16:15:46 +0200

    more tests added

--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -13,7 +13,7 @@
 #include "easyfind.hpp"
 #include "colours.hpp"
 #include <vector>
-
+#include <list>
 
 int main(void)
 {
@@ -50,5 +50,38 @@ int main(void)
 	{
 		std::cerr << RED << "Caught an exception: " << e.what() << RESET << '\n';
 	}
+	std::cout << "\nTest num 3. We're trying to find number 3 in a list container ";
+	std::cout << "that has the numbers from 1 to 10." << std::endl;
+	try
+	{
+		std::list<int>	numbers;
+		for (int i = 0; i < 10; ++i)
+			numbers.push_back(i);
+		int position = easyfind(numbers, 3);
+
+		std::cout << GREEN << "The value was found in position num: " << position;
+		std::cout << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << "Caught an exception: " << e.what() << RESET << '\n';
+	}
+	std::cout << "\nTest num 4. We're trying to find number 666 in a list container ";
+	std::cout << "that has the numbers from 1 to 10." << std::endl;
+	try
+	{
+		std::list<int>	numbers;
+		for (int i = 0; i < 10; ++i)
+			numbers.push_back(i);
+		int position = easyfind(numbers, 666);
+
+		std::cout << GREEN << "The value was found in position num: " << position;
+		std::cout << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << "Caught an exception: " << e.what() << RESET << '\n';
+	}
+	std::cout << BLUE << "\n Thanks for using the service. Come back soon! 😃" << RESET << std::endl;
 	return(0);
 }
