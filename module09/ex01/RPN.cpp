d7758376bb75b01bd17dc0981f07e80b6dbcaa16
Marti Marsa Samper <mmarsa-s@cbr2s3.42barcelona.com>
2025-07-17 19:50:28 +0200

    feat: add initial RPN module and build files

---
+++ module09/ex01/RPN.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmarsa-s <mmarsa-s@student.42fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 19:30:36 by mmarsa-s          #+#    #+#             */
+/*   Updated: 2025/07/17 19:30:40 by mmarsa-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.hpp"
+
+RPN::RPN() {}
+
+RPN::~RPN() {}
+
+RPN::RPN(const RPN &other) { *this = other; }
+
+RPN &RPN::operator=(const RPN &other)
+{
+    if (this != &other)
+        this->_stack = other._stack;
+    return *this;
+}
---
+++ module09/ex01/RPN.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmarsa-s <mmarsa-s@student.42fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 19:20:50 by mmarsa-s          #+#    #+#             */
+/*   Updated: 2025/07/17 19:20:52 by mmarsa-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+# define RPN_HPP
+
+#include <stack>
+#include <iostream>
+
+class RPN
+{
+private:
+    std::stack<int> _stack;
+public:
+    RPN();
+    ~RPN();
+    RPN(const RPN &other);
+	RPN &operator=(const RPN &other);
+};
+
+#endif
